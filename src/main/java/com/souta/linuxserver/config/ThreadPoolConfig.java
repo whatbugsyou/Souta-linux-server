6a9813144cfd9d6f565baf3da93c5a09b5f713a3
生气气 <1050466416@qq.com>
2021-08-04 18:03:37 +0800

    update: remove guava dependency

--- src/main/java/com/souta/linuxserver/config/ThreadPoolConfig.java
+++ src/main/java/com/souta/linuxserver/config/ThreadPoolConfig.java
@@ -1,6 +1,6 @@
 package com.souta.linuxserver.config;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import cn.hutool.core.thread.NamedThreadFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,56 +12,57 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class ThreadPoolConfig {
     @Bean("dialingPool")
-    public ExecutorService dialingPool(){
-        ThreadFactoryBuilder threadFactoryBuilder = new ThreadFactoryBuilder().setNameFormat("dialing-pool-%d");
+    public ExecutorService dialingPool() {
+        NamedThreadFactory namedThreadFactory = new NamedThreadFactory("dialing-pool-", false);
         return new ThreadPoolExecutor(100,
                 100,
                 60,
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(),
-                threadFactoryBuilder.build());
+                namedThreadFactory);
     }
 
     @Bean("refreshPool")
-    public ExecutorService flushLineRetryPool(){
-        ThreadFactoryBuilder threadFactoryBuilder = new ThreadFactoryBuilder().setNameFormat("refresh-pool-%d");
+    public ExecutorService flushLineRetryPool() {
+        NamedThreadFactory namedThreadFactory = new NamedThreadFactory("refresh-pool-", false);
         return new ThreadPoolExecutor(100,
                 100,
                 60,
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(),
-                threadFactoryBuilder.build());
+                namedThreadFactory);
     }
 
     @Bean("netPool")
-    public ExecutorService netPool(){
-        ThreadFactoryBuilder threadFactoryBuilder = new ThreadFactoryBuilder().setNameFormat("net-pool-%d");
+    public ExecutorService netPool() {
+        NamedThreadFactory namedThreadFactory = new NamedThreadFactory("net-pool-", false);
         return new ThreadPoolExecutor(20,
                 20,
                 60,
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(),
-                threadFactoryBuilder.build());
+                namedThreadFactory);
     }
 
     @Bean("basePool")
-    public ExecutorService basePool(){
-        ThreadFactoryBuilder threadFactoryBuilder = new ThreadFactoryBuilder().setNameFormat("base-pool-%d");
+    public ExecutorService basePool() {
+        NamedThreadFactory namedThreadFactory = new NamedThreadFactory("base-pool-", false);
         return new ThreadPoolExecutor(20,
                 20,
                 60,
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(),
-                threadFactoryBuilder.build());
+                namedThreadFactory);
     }
+
     @Bean("linePool")
-    public ExecutorService linePool(){
-        ThreadFactoryBuilder threadFactoryBuilder = new ThreadFactoryBuilder().setNameFormat("line-pool-%d");
+    public ExecutorService linePool() {
+        NamedThreadFactory namedThreadFactory = new NamedThreadFactory("line-pool-", false);
         return new ThreadPoolExecutor(100,
                 100,
                 60,
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>(),
-                threadFactoryBuilder.build());
+                namedThreadFactory);
     }
 }
