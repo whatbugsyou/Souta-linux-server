507f3539f26882e6391b9d8eb01d4697e000e7fc
whatbugsyou <1050466416@qq.com>
2021-10-21 20:21:08 +0800

    update: server init

--- src/main/java/com/souta/linuxserver/controller/Socks5Controller.java
+++ src/main/java/com/souta/linuxserver/controller/Socks5Controller.java
@@ -1,8 +1,16 @@
 package com.souta.linuxserver.controller;
 
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.souta.linuxserver.dto.Socks5Info;
+import com.souta.linuxserver.exception.ResponseNotOkException;
 import com.souta.linuxserver.service.Socks5Service;
+import com.souta.linuxserver.util.FileUtil;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/v1.0/line/socks5")
@@ -13,11 +21,27 @@ public class Socks5Controller {
         this.socks5Service = socks5Service;
     }
 
+    @PostConstruct()
+    public void init() {
+        Object socks5Info = getSocks5Info("");
+        try {
+            HttpResponse execute = HttpRequest
+                    .post(Host.java_server_host + "/v1.0/line")
+                    .body(JSON.toJSONString(socks5Info), "application/json;charset=UTF-8")
+                    .execute();
+            if (execute.getStatus() != 200) {
+                throw new ResponseNotOkException("error in sending registerHost from java server,API(PUT) :  /v1.0/server");
+            }
+        } catch (ResponseNotOkException e) {
+            e.printStackTrace();
+        }
+    }
+
     @GetMapping("info")
     public Object getSocks5Info(@RequestParam(value = "ip", defaultValue = "") String ip) {
         if (ip.equals("")) {
             return socks5Service.getAllSocks5();
-        }else {
+        } else {
             return socks5Service.getSocks5(ip);
         }
     }
