999b5f59204047fea67476483840ee88920e0f47
whatbugsyou <1050466416@qq.com>
2021-10-21 22:48:45 +0800

    update: random auth

--- src/main/java/com/souta/linuxserver/dto/Socks5Info.java
+++ src/main/java/com/souta/linuxserver/dto/Socks5Info.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Data
 @NoArgsConstructor
@@ -20,10 +21,20 @@ public class Socks5Info {
         this.port = 18000;
         this.status = false;
         AuthInfo authInfo = new AuthInfo();
-        authInfo.setUsername("test");
-        authInfo.setPassword("test");
+        authInfo.setUsername(getRandomString(8));
+        authInfo.setPassword(getRandomString(8));
         this.authList = Arrays.asList(authInfo);
     }
 
+    public static String getRandomString(int length){
+        String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random=new Random();
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<length;i++){
+            int number=random.nextInt(62);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
+    }
 
 }
